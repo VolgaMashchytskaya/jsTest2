d6c40bbd588a1e08e4a06b1737a6893e236827f2
Volga <volga.mashchytskaya@gmail.com>
2024-07-22 15:38:17 +0300

    first version

---
+++ js.js
@@ -0,0 +1,45 @@
+//Выведите в консоль номер месяца в котором вы родились. Изучите чем отличается вывод числа и строки.
+
+console.log(6)
+console.log('6')
+var a = 6
+var b = '6'
+console.log(a)
+console.log(b)
+console.log(typeof(a))
+console.log(typeof(b))
+
+//Выведите в консоль строку: “ 'Добро '+'пожаловать '+' на курс' '+' ' Ваше имя' ”
+
+let c
+c ='Добро '
+var d = 'пожаловать ' 
+var e = ' на курс,'
+const myName = ' Ольга'
+const result = `Добро пожаловать на курс, ${myName}`
+
+console.log(c + d + e + myName)
+console.log(result)
+
+
+//С помощью “Alert” выведите день месяц и год сегодняшний, после теста, закомментируйте код, чтобы он не мешал дальше выполнять практику
+
+//alert( new Date());
+
+//Создайте на странице div с id=one. С помощью document.getElementById запишите в данный элемент текст 'Hello World'.
+
+let add = document.getElementById("one").innerHTML = 'Hello World'
+
+
+//Выведите все типы данных как было сделано на уроке в консоль.
+
+console.log(typeof(10))
+console.log(typeof('лето'))
+console.log(typeof(10n))
+console.log(typeof(false))
+console.log(typeof(null))
+console.log(typeof(undefined))
+
+
+//Добавьте эти данные в свой github как в 1 занятии
+
